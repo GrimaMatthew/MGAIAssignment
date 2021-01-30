82a88d47b139ded08b983c06abc8e43e0c850fb3
Matthew Grima <matthew@Matthews-MacBook-Pro.local>
2021-01-30 23:20:30 +0100

    Moving obstacles from one point to the next instead of randomly on the screen

--- Scripts/moveObstacle.cs
+++ Scripts/moveObstacle.cs
@@ -5,36 +5,74 @@ using UnityEngine;
 public class moveObstacle : MonoBehaviour
 {
 
-    public List<Transform> waypoints;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(moveMe());
-        
+        StartCoroutine(moveObsti());
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-
-    IEnumerator moveMe()
+    IEnumerator moveObsti()
     {
-        while(true)
-        {
-            
-           
+        //create a new list of positions.
+        List<Vector3> positions = new List<Vector3>();
 
-            this.transform.position = new Vector3(Random.Range(-20f,50f), Random.Range(-20f, 50f), 1);
-            yield return new WaitForSeconds(5f);
+        positions.Add(this.transform.position);
 
-            
-      
-        }
-      
+        positions.Add(new Vector3(this.transform.position.x, -this.transform.position.y));
+
+        StartCoroutine(moveObsti(this.transform, positions, true));
+
+
+
+        yield return null;
     }
 
+    IEnumerator moveObsti(Transform t, List<Vector3> points, bool loop)
+    {
+        if (loop)
+        {
+            //needs to run indefinitely
+            while (true)
+            {
+                List<Vector3> forwardpoints = points;
 
+                foreach (Vector3 position in forwardpoints)
+                {
+                    while (Vector3.Distance(t.position, position) > 0.5f)
+                    {
+                        t.position = Vector3.MoveTowards(t.position, position, 1f);
+                        Debug.Log(position);/**/
+                        yield return new WaitForSeconds(0.2f);
+                    }
+                }
+                //reverse the points supplied here
+                forwardpoints.Reverse();
+                yield return null;
+
+            }
+        }
+        else
+        {
+            foreach (Vector3 position in points)
+            {
+                while (Vector3.Distance(t.position, position) > 0.5f)
+                {
+                    t.position = Vector3.MoveTowards(t.position, position, 1f);
+                    /**/
+                    yield return new WaitForSeconds(0.2f);
+                }
+            }
+            yield return null;
+        }
+
+
+    }
 }
